2d838f1466ea901167e101c6b45c2958d124ad33
nandishhg <nandishgowdahg10@gmail.com>
2025-12-01 10:32:23 +0530

    Add files via upload

---
+++ course.py
@@ -0,0 +1,32 @@
+print('select your choice')
+print('1.after 10th')
+print('2.after 12th')
+print('3.after graduation')
+while true:
+    choice == input(1/2/3)
+    if choice in(1/2/3):
+        try:
+            print('which are thr option are avialabe')
+        except valueError:
+            print('invalid input')
+            continue:
+        if choice =='1':
+                    print('can do puc')
+        elif choice =='2':
+                    print('can do bca')
+        elif choice =='3':
+              prinrt('can do masters')
+        next_toDo=input('next information needed (yes/no)')
+        if next_toDo=='no'
+        break
+    else:
+     print('invalid input')
+                    
+                   
+
+print 123
+if choice456
+
+                    
+    
+    
